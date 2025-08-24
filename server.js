0e5b2e95d923cf4c55ebd76c01085eeae8bb2fb6
YCE-Andy <Andy@andygannon.co.uk>
2025-08-24 15:22:35 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const SECRET = process.env.SECRET || "changeme";
+
+// TradingView will POST alerts here
+app.post("/tv", async (req, res) => {
+  try {
+    if (req.headers["x-secret"] !== SECRET) {
+      return res.status(403).send("Forbidden");
+    }
+
+    const alert = req.body;
+    console.log("Received alert:", alert);
+
+    await axios.post(DISCORD_WEBHOOK_URL, {
+      content: `📈 TradingView Alert:\n${JSON.stringify(alert, null, 2)}`
+    });
+
+    res.status(200).send("ok");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
